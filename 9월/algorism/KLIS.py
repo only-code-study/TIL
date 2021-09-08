20afc492cad540da277b0170ae6ced8b6c01c3f1
oripk <cotmd6203@naver.com>
2021-09-08 03:26:35 +0000

    9월 8일

--- 9월/algorism/KLIS.py
+++ 9월/algorism/KLIS.py
@@ -1,24 +1,31 @@
-import math
-
 C = int(input())
-casheLen = [-1] * 501
-cacheCnt = [-1] * 501
-S = [-1] * 501
 
-def lis(arr):
-    if not arr:
-        return 0
-    
-    ret = 1
-    for i in range(len(arr)):
-        nxt = []
-        for j in range(i+1, len(arr)):
-            if arr[i] < arr[j]:
-                nxt.append(arr[j])
-        ret = max(ret, 1 + lis(nxt))
-    return ret	
+def solve():
+    global N, arr
+    tmpArr = []
+    tmpArr.append(arr[0])
+    maxLen = 1
+    for i in arr[1:]:
+        index = binary_search(tmpArr, i)
+        tmpArr = tmpArr[:index]
+        tmpArr.append(i) 
+        print(tmpArr)
+        maxLen = max(maxLen, len(tmpArr))
+    return maxLen
+
+def binary_search(arr, x):
+    low = 0
+    high = len(arr)
+    mid = 0
+    while low < high:
+        mid = (high + low) // 2
+        if arr[mid] > x:
+            high = mid
+        else:
+            low = mid + 1
+    return high
 
 for i in range(C):
     N, K = map(int, input().split())
     arr = list(map(int, input().split()))
-    print('#', lis(arr))
+    print('#', solve())
