7f414412201fd1094280910915095a8036dddaf7
oripk <fine6115@gmail.com>
2021-09-09 13:25:52 +0900

    9월 9일

--- 9월/algorism/KLIS.py
+++ 9월/algorism/KLIS.py
@@ -1,29 +1,33 @@
 C = int(input())
 
-def solve():
-    global N, arr
-    tmpArr = []
-    tmpArr.append(arr[0])
-    maxLen = 1
-    for i in arr[1:]:
-        index = binary_search(tmpArr, i)
-        tmpArr = tmpArr[:index]
-        tmpArr.append(i) 
-        print(tmpArr)
-        maxLen = max(maxLen, len(tmpArr))
-    return maxLen
+# def solve():
+#     global N, arr
+#     tmpArr = []
+#     tmpArr.append(arr[0])
+#     maxLen = 1
+#     for i in arr[1:]:
+#         index = binary_search(tmpArr, i)
+#         tmpArr = tmpArr[:index]
+#         tmpArr.append(i) 
+#         print(tmpArr)
+#         maxLen = max(maxLen, len(tmpArr))
+#     return maxLen
 
-def binary_search(arr, x):
-    low = 0
-    high = len(arr)
-    mid = 0
-    while low < high:
-        mid = (high + low) // 2
-        if arr[mid] > x:
-            high = mid
-        else:
-            low = mid + 1
-    return high
+# def binary_search(arr, x):
+#     low = 0
+#     high = len(arr)
+#     mid = 0
+#     while low < high:
+#         mid = (high + low) // 2
+#         if arr[mid] > x:
+#             high = mid
+#         else:
+#             low = mid + 1
+#     return high
+
+def solve():
+    
+    return
 
 for i in range(C):
     N, K = map(int, input().split())
